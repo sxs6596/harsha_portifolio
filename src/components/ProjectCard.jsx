4c41df74ad33977e6938177ccb5feacc004e761b
sxs6596 <sxs6596@mavs.uta.edu>
2024-07-21 15:21:28 -0500

    added storymatic and echomeet projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-import UTALogo from "../assets/uta_logo.png";
-import GameNexus from "../assets/game_nexus.png";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import BadgeList from "./BadgeList";
-const ProjectCard = ({title,link,skills,image}) => {
-//   const skills = ['Java', 'Python', 'Javascript', 'React.js' ]; 
+
+const ProjectCard = ({ title, link, skills, image, content }) => {
   return (
     <div className="max-w-3xl rounded-lg shadow-sm hover:bg-leftpanel-bg py-4 px-3 m-3 mx-auto">
-        <div className="flex flex-col space-y-4 my-4 items-center justify-center md:flex-row md:justify-between">
-          <img src={image} alt="" className="w-1/2 rounded-lg transition transform hover:scale-105" />
-          <div className="flex flex-col space-y-2">
-                    <h4 className="text-xl text-slate-700 font-semibold">
-                      {title} <a href={link}className="mx-2 inline-block"><FaExternalLinkAlt /></a>
-                    </h4>
-                <p><BadgeList items={skills}/></p>      
-              
-            
+      <div className="flex flex-col space-y-4 my-4 items-center justify-center md:flex-row md:justify-between flex-wrap">
+        <img 
+          src={image} 
+          alt="" 
+          className="w-70 h-40 rounded-lg transition transform hover:scale-110 mx-auto"
+        />
+        <div className="flex flex-col space-y-2 w-full">
+          <h4 className="text-xl text-slate-700 font-semibold text-center">
+            {title}
+            <a href={link} className="mx-2 inline-block">
+              <FaExternalLinkAlt />
+            </a>
+          </h4>
+          <p className="text-slate-700 text-center text-sm font-semibold">{content}</p>
+          <div className="flex justify-end">
+            <BadgeList items={skills} />
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,9 @@ import UTALogo from '../assets/uta_logo.png';
 import ProjectCard from './ProjectCard';
 import GameNexus from '../assets/game_nexus.png';
 import CampusFlow from '../assets/campus_flow.png';
+import storymatic from '../assets/storymatic.png';
 import IMDB from '../assets/IMDB.png';
+import EchoMeet from '../assets/EchoMeet.png';
 import IssueTracker from '../assets/IssueTracker.png';
 const Projects = () => {
   const link = 'https://game-nexus-pearl.vercel.app/';
@@ -14,10 +16,14 @@ const Projects = () => {
          <h2 className="text-3xl font-semibold text-slate-700 text-center p-3 m-3">
         Projects
        </h2>
-        <ProjectCard title={title} skills={skills} link={link} image={GameNexus} />
-        <ProjectCard title={'IssueTracker'} skills={['Next.js', 'MySQL', 'Tailwind', 'Prisma']} link={'https://github.com/sxs6596/IssueTracker'} image={IssueTracker} />
-        <ProjectCard title={'IMDB Sentiment Analyzer'} skills={['Next.js', 'ML', 'Hybrid', 'DeepLearning']} link={'https://github.com/sxs6596/IMDB-Sentiment-Analysis'} image={IMDB} />
-        <ProjectCard title={'Campus Flow'} skills={['React.js', 'PHP', 'Radix UI', 'Open AI']} link={'https://github.com/sxs6596/campusFlow'} image={CampusFlow}/>
+      
+        <ProjectCard title={'StoryMatic AI'} skills={['Next.js', 'PostgreSQL', 'Stripe', 'Drizzle ORM']} link={'https://storymatic.vercel.app/'} image={storymatic} content={'FullStack application built for AI content generation using Gemini flash.'} />
+        <ProjectCard title={'Echo Meet'} skills={['React.js', 'Node.js', 'WebRTC', 'Socket.io']} link={'https://echo-meet-sigma.vercel.app/'} image={EchoMeet}  content={'peer to peer AI video calling application.'}/>
+        <ProjectCard title={title} skills={skills} link={link} image={GameNexus} content={'Full Stack video gaming platform built for discovering video games'} />
+        <ProjectCard title={'Bug Breeze'} skills={['Next.js', 'MySQL', 'Tailwind', 'Prisma']} link={'https://github.com/sxs6596/IssueTracker'} image={IssueTracker} content={'SEO, SSR enabled Issue Tracker built for secure workflow.'} />
+        <ProjectCard title={'IMDB Sentiment Analyzer'} skills={['Next.js', 'ML', 'Hybrid', 'DeepLearning']} link={'https://github.com/sxs6596/IMDB-Sentiment-Analysis'} image={IMDB} content={'Realtime Sentiment Analysis Dashboard built using Hybrid model of CNN & RNN models.'}/>
+        <ProjectCard title={'Campus Flow'} skills={['React.js', 'PHP', 'Radix UI', 'Open AI']} link={'https://github.com/sxs6596/campusFlow'} image={CampusFlow} content={'FullStack application built for schools with OPEN AI integration for student, faculty & admin dashboards'}/>
+        
     </div>
   )
 }
